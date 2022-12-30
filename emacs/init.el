66d9d225fda65ca953929b8903acc0b19dca7e4a
Bee <bee128029@protonmail.com>
2022-12-29 18:24:45 -0800

    ¯\_(ツ)_/¯

---
+++ emacs/init.el
@@ -0,0 +1,48 @@
+(package-initialize)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(add-to-list 'load-path "~/.config/emacs/packages/")
+(add-to-list 'load-path "~/.config/emacs/packages/evil")
+
+(setq package-enable-at-startup nil)
+
+(require 'evil)
+(evil-mode t)
+
+(require 'package)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'evil)
+  (package-refresh-contents)
+  (package-install 'evil))
+
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(set-frame-font "JetBrains Mono-14")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("7923541211298e4fd1db76c388b1d2cb10f6a5c853c3da9b9c46a02b7f78c882" default))
+ '(ispell-dictionary nil)
+ '(package-selected-packages '(gruber-darker-theme smex evil use-package cmake-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
