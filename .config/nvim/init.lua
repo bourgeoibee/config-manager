85e410e14861af5a61d23ba308ccdb00135c2119
Bee <bee128029@protonmail.com>
2022-12-30 07:51:59 -0800

    Move options to options.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,26 +2,8 @@ vim.g.mapleader = ' '
 
 vim.cmd.colorscheme('melange')
 
-local opt = vim.opt
+require('options')
 
-opt.softtabstop = 8 -- Tab length
-opt.shiftwidth = 8 -- Indentation length
-opt.expandtab = true -- Expand tabs to spaces
-opt.number = true -- Line numbers on left
-opt.relativenumber = true
-opt.incsearch = true -- Highlight as you search
-opt.hlsearch = false -- Don't highlight after search
-opt.errorbells = false -- No annoying error bell sounds
-opt.visualbell = true
-opt.wrap = false -- Disable line wrapping
---opt.cursorline = true  -- Underline the line the cursor is on
-opt.scrolloff = 8 -- Keep 8 lines above and below cursor
-opt.autoindent = true -- Copy indentation of current line onto new line
-opt.smartindent = true -- Indents are inserted based on syntax
-opt.updatetime = 300 -- How many milliseconds inbetween swap file updates
-opt.termguicolors = true -- 24-bit RGB color TUI
-opt.completeopt = { "menu", "menuone", "noselect" } -- ???
-opt.mouse = ""
 
 -- Plugins
 local fn = vim.fn
