607a54c9c11831115000b72d89ac539387a1abaf
Bee <bee128029@protonmail.com>
2023-02-17 17:12:22 -0800

    Mess with stuff idk emacs

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,39 +1,47 @@
-(package-initialize)
-
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(add-to-list 'load-path "~/.config/emacs/packages/")
-
-(setq package-enable-at-startup nil)
-
-(require 'package)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(setq package-enable-at-startup nil
+      inhibit-startup-screen t
+      read-file-name-completion-ignore-case t
+      c-default-style "linux"
+      custom-safe-themes t)
+(setq-default display-line-numbers 'relative)
 
 (menu-bar-mode 1)
 (tool-bar-mode 0)
 (toggle-frame-maximized)
 (set-frame-font "JetBrains Mono-14")
+(load-theme 'gruber-darker t)
+(show-paren-mode t)
+
+(package-initialize)
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+;;(add-to-list 'load-path "~/.config/emacs/packages/")
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+;;(use-package evil)
+(use-package rust-mode)
+(use-package lsp-mode)
+(use-package magit)
+(use-package tree-sitter)
+(use-package gruber-darker-theme)
+;;(use-package smex)
+(use-package cmake-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes '(gruber-darker))
- '(custom-safe-themes
-   '("7923541211298e4fd1db76c388b1d2cb10f6a5c853c3da9b9c46a02b7f78c882" default))
- '(ispell-dictionary nil)
  '(package-selected-packages
-   '(magit tree-sitter gruber-darker-theme smex evil use-package cmake-mode)))
+   '(gruber-darker-theme cmake-mode use-package tree-sitter rust-mode magit lsp-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
